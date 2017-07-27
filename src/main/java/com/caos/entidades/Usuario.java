b962e4ea292ed716a93d926a24d140a263f093bd
Carlos Arturo Angarita Peñaloza <carlomagno127@hotmail.com>
2017-07-26 22:21:00 -0500

    creada clase que describe a un usuario

---
+++ src/main/java/com/caos/entidades/Usuario.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.caos.entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+/**
+ * Clase que define lo que es un Usuario del Sistema
+ *
+ * @author CarlosArturo
+ */
+@Entity
+@Table(name = "USUARIOS")
+public class Usuario implements Serializable {
+
+    @Id
+    private String username;
+
+    private String password;
+
+    @Column(name = "fecha_creacion")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fechaCreacion;
+
+    @Column(name = "ultimo_acceso")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date ultimoAcceso;
+
+    @Column(name = "estado_sesion")
+    private String estadoSesion;
+
+    @Column(name = "rol_id")
+    private String rol;
+
+    private String nombre;
+    private String email;
+
+    public Usuario() {
+    }
+
+    public Usuario(String username, String password, Date fechaCreacion, Date ultimoAcceso, String estadoSesion, String rol, String nombre, String email) {
+        this.username = username;
+        this.password = password;
+        this.fechaCreacion = fechaCreacion;
+        this.ultimoAcceso = ultimoAcceso;
+        this.estadoSesion = estadoSesion;
+        this.rol = rol;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getUltimoAcceso() {
+        return ultimoAcceso;
+    }
+
+    public void setUltimoAcceso(Date ultimoAcceso) {
+        this.ultimoAcceso = ultimoAcceso;
+    }
+
+    public String getEstadoSesion() {
+        return estadoSesion;
+    }
+
+    public void setEstadoSesion(String estadoSesion) {
+        this.estadoSesion = estadoSesion;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.username);
+        hash = 17 * hash + Objects.hashCode(this.password);
+        hash = 17 * hash + Objects.hashCode(this.fechaCreacion);
+        hash = 17 * hash + Objects.hashCode(this.rol);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        if (!Objects.equals(this.fechaCreacion, other.fechaCreacion)) {
+            return false;
+        }
+        if (!Objects.equals(this.rol, other.rol)) {
+            return false;
+        }
+        return true;
+    }
+
+
+}
